77681caabf32eee672fc282a5285b14c3d9e39f7
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-09-23 18:50:03 +0700

    official

--- uninstall/debian-bullseye.sh
+++ uninstall/debian-bullseye.sh
@@ -3,7 +3,7 @@ if [ -d "$PREFIX/share/debian-bullseye" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling Debian Bullseye, please wait...\n\e[31m"
 rm -rf "$PREFIX/share/debian-bullseye"
 rm -f "$PREFIX/bin/start-debian-bullseye"
-printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstalled successfully.\n\n\e[0m"
+printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
 printf "\n\e[31mError: distribution Debian Bullseye is not installed.\n\n\e[0m"
