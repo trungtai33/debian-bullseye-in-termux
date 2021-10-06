a9bd21ab2a93bcc19dffd2144699163da08f9eca
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-06 07:06:29 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+directory="debian-bullseye"
 distro_name="Debian Bullseye"
-if [ -d "${PREFIX}/share/debian-bullseye" ]; then
+if [ -d "${PREFIX}/share/${directory}" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
-rm -rf "${PREFIX}/share/debian-bullseye"
-rm -f "${PREFIX}/bin/start-debian-bullseye"
+rm -rf "${PREFIX}/share/${directory}"
+rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
